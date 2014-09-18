9ac87dadc279f49dcd22ab113d594ff879f2a39e
Krzysztof Langner <klangner@gmail.com>
2014-09-18 22:00:44 +0200

    Added exceptions to electron configuration Added ionization and electron negativity with same data

--- src/Chemistry/Element.hs
+++ src/Chemistry/Element.hs
@@ -11,128 +11,133 @@ data Element = Element { atomicNumber :: Int
                        , symbol :: String
                        , _name :: String
                        , _weight :: Double 
+                       , _ionizationEnergy :: Double
+                       , _electroNeg :: Double
+                       , _electronConfig :: [Int]
                        } 
              | Unknown deriving (Eq, Show)
             
+-- Periodic Table
+-- Electron configuration is only provided for elements which are exception to Aufbau principle.             
 ptable :: [Element]
-ptable = [ Element 1 "H" "Hydrogen" 1.008 
-         , Element 2 "He" "Helium" 4.002602 
-         , Element 3 "Li" "Lithium" 6.941 
-         , Element 4 "Be" "Beryllium" 9.012182 
-         , Element 5 "B" "Boron" 10.811 
-         , Element 6 "C" "Carbon" 12.011 
-         , Element 7 "N" "Nitrogen" 14.007 
-         , Element 8 "O" "Oxygen" 15.999
-         , Element 9 "F" "Fluorine" 18.9984032 
-         , Element 10 "Ne" "Neon" 20.1797 
-         , Element 11 "Na" "Sodium" 22.989768 
-         , Element 12 "Mg" "Magnesium" 24.305 
-         , Element 13 "Al" "Alluminium" 26.981
-         , Element 14 "Si" "Silicon" 28.085
-         , Element 15 "P" "Phosphous" 30.973
-         , Element 16 "S" "Sulphur" 32.066 
-         , Element 17 "Cl" "Chlorine" 35.4527 
-         , Element 18 "Ar" "Argon" 39.948 
-         , Element 19 "K" "Potassium" 39.0983 
-         , Element 20 "Ca" "Calcium" 40.078 
-         , Element 21 "Sc" "Scandium" 44.95591 
-         , Element 22 "Ti" "Titanium" 47.88 
-         , Element 23 "V" "Vanadium" 50.9415 
-         , Element 24 "Cr" "Chromium" 51.9961 
-         , Element 25 "Mn" "Manganese" 54.93805 
-         , Element 26 "Fe" "Iron" 55.845
-         , Element 27 "Co" "Cobalt" 58.933
-         , Element 28 "Ni" "Nickel" 58.6934
-         , Element 29 "Cu" "Copper" 63.546
-         , Element 30 "Zn" "Zinc" 65.39 
-         , Element 31 "Ga" "Gallium" 69.723 
-         , Element 32 "Ge" "Germanium" 72.61 
-         , Element 33 "As" "Arsenic" 74.92159 
-         , Element 34 "Se" "Selenium" 78.96 
-         , Element 35 "Br" "Bromine" 79.904 
-         , Element 36 "Kr" "Krypton" 83.8 
-         , Element 37 "Rb" "Rubidium" 85.4678 
-         , Element 38 "Sr" "Strontium" 87.62 
-         , Element 39 "Y" "Yttrium" 88.90585 
-         , Element 40 "Zr" "Zirconium" 91.224 
-         , Element 41 "Nb" "Niobium" 92.90638 
-         , Element 42 "Mo" "Molybdenum" 95.94 
-         , Element 43 "Tc" "Technetium" 98.9063 
-         , Element 44 "Ru" "Ruthenium" 101.07 
-         , Element 45 "Rh" "Rhodium" 102.9055 
-         , Element 46 "Pd" "Palladium" 106.42 
-         , Element 47 "Ag" "Silver" 107.8682 
-         , Element 48 "Cd" "Cadmium" 112.411 
-         , Element 49 "In" "Indium" 114.82 
-         , Element 50 "Sn" "Tin" 118.71 
-         , Element 51 "Sb" "Antimony" 121.75 
-         , Element 52 "Te" "Tellurium" 127.6 
-         , Element 53 "I" "Iodine" 126.90447 
-         , Element 54 "Xe" "Xenon" 131.29 
-         , Element 55 "Cs" "Caesium" 132.90543 
-         , Element 56 "Ba" "Barium" 137.327 
-         , Element 57 "La" "Lanthanum" 138.9055 
-         , Element 58 "Ce" "Cerium" 140.115 
-         , Element 59 "Pr" "Praseodymium" 140.90765 
-         , Element 60 "Nd" "Neodymium" 144.24 
-         , Element 61 "Pm" "Promethium" 146.9151 
-         , Element 62 "Sm" "Samarium" 150.36 
-         , Element 63 "Eu" "Europium" 151.965 
-         , Element 64 "Gd" "Gadolinium" 157.25 
-         , Element 65 "Tb" "Terbium" 158.92534 
-         , Element 66 "Dy" "Dysprosium" 162.5 
-         , Element 67 "Ho" "Holmium" 164.93032 
-         , Element 68 "Er" "Erbium" 167.26 
-         , Element 69 "Tm" "Thulium" 168.93421 
-         , Element 70 "Yb" "Ytterbium" 173.04 
-         , Element 71 "Lu" "Lutetium" 174.967 
-         , Element 72 "Hf" "Hafnium" 178.49 
-         , Element 73 "Ta" "Tantalum" 180.9479 
-         , Element 74 "W" "Tungsten" 183.85 
-         , Element 75 "Re" "Rhenium" 186.207 
-         , Element 76 "Os" "Osmium" 190.2 
-         , Element 77 "Ir" "Iridium" 192.22 
-         , Element 78 "Pt" "Platinum" 195.08 
-         , Element 79 "Au" "Gold" 196.96654  
-         , Element 80 "Hg" "Mercury" 200.59  
-         , Element 81 "Tl" "Thallium" 204.3833 
-         , Element 82 "Pb" "Lead" 207.2 
-         , Element 83 "Bi" "Bismuth" 208.98037 
-         , Element 84 "Po" "Polonium" 208.9824 
-         , Element 85 "At" "Astatine" 209.9871 
-         , Element 86 "Rn" "Radon" 222.0176 
-         , Element 87 "Fr" "Francium" 223.0197 
-         , Element 88 "Ra" "Radium" 226.0254 
-         , Element 89 "Ac" "Actinium" 227.0278 
-         , Element 90 "Th" "Thorium" 232.0381 
-         , Element 91 "Pa" "Protactinium" 231.0359 
-         , Element 92 "U" "Uranium" 238.0289 
-         , Element 93 "Np" "Neptunium" 237.0482 
-         , Element 94 "Pu" "Plutonium" 244.0642 
-         , Element 95 "Am" "Americium" 243.0614 
-         , Element 96 "Cm" "Curium" 247.0703 
-         , Element 97 "Bk" "Berkelium" 247.0703 
-         , Element 98 "Cf" "Californium" 251.0796 
-         , Element 99 "Es" "Einsteinium" 252.0829 
-         , Element 100 "Fm" "Fermium" 257.0951 
-         , Element 101 "Md" "Mendelevium" 258.0986 
-         , Element 102 "No" "Nobelium" 259.1009 
-         , Element 103 "Lr" "Lawrencium" 260.1053 
-         , Element 104 "Rf" "Rutherfordium" 261.1087 
-         , Element 105 "Db" "Dubnium" 262.1138 
-         , Element 106 "Sg" "Seaborgium" 263.1182 
-         , Element 107 "Bh" "Bohrium" 262.1229 
-         , Element 108 "Hs" "Hassium" 265 
-         , Element 109 "Mt" "Meitnerium" 266 
-         , Element 110 "Ds" "Darmstadtium" 269 
-         , Element 111 "Rg" "Roentgenium" 272 
-         , Element 112 "Cn" "Copernicium" 285 
-         , Element 113 "Uut" "Ununtrium" 284 
-         , Element 114 "Uug" "Ununquadium" 289 
-         , Element 115 "Uup" "Ununpentium" 288 
-         , Element 116 "Uuh" "Ununhexium" 293 
-         , Element 117 "Uus" "Ununseptium" 294     
-         , Element 118 "Uuo" "Ununoctium" 294 ]
+ptable = [ Element 1 "H" "Hydrogen"     1.008       1.312   2.2     []
+         , Element 2 "He" "Helium"      4.002602    2372.3  0       []
+         , Element 3 "Li" "Lithium"     6.941       520.2   0.98    []
+         , Element 4 "Be" "Beryllium"   9.012182    899.5   1.57    []
+         , Element 5 "B" "Boron"        10.811      800.6   2.04    []
+         , Element 6 "C" "Carbon"       12.011      1086.5  2.55    []
+         , Element 7 "N" "Nitrogen"     14.007      1402.3  3.04    []
+         , Element 8 "O" "Oxygen"       15.999      1313.9  3.44    []
+         , Element 9 "F" "Fluorine"     18.9984032  1681    3.98    []
+         , Element 10 "Ne" "Neon"       20.1797     2080.7  0       []
+         , Element 11 "Na" "Sodium" 22.989768 0 0 []
+         , Element 12 "Mg" "Magnesium" 24.305 0 0 []
+         , Element 13 "Al" "Alluminium" 26.981 0 0 []
+         , Element 14 "Si" "Silicon" 28.085 0 0 []
+         , Element 15 "P" "Phosphous" 30.973 0 0 []
+         , Element 16 "S" "Sulphur" 32.066 0 0 []
+         , Element 17 "Cl" "Chlorine" 35.4527 0 0 []
+         , Element 18 "Ar" "Argon" 39.948 0 0 []
+         , Element 19 "K" "Potassium" 39.0983 0 0 []
+         , Element 20 "Ca" "Calcium" 40.078 0 0 []
+         , Element 21 "Sc" "Scandium" 44.95591 0 0 []
+         , Element 22 "Ti" "Titanium" 47.88 0 0 []
+         , Element 23 "V" "Vanadium" 50.9415 0 0 []
+         , Element 24 "Cr" "Chromium" 51.9961 0 0 [2, 8, 13, 1]
+         , Element 25 "Mn" "Manganese" 54.93805 0 0 []
+         , Element 26 "Fe" "Iron" 55.845 0 0 []
+         , Element 27 "Co" "Cobalt" 58.933 0 0 []
+         , Element 28 "Ni" "Nickel" 58.6934 0 0 []
+         , Element 29 "Cu" "Copper" 63.546 0 0 [2, 8, 18, 1]
+         , Element 30 "Zn" "Zinc" 65.39 0 0 []
+         , Element 31 "Ga" "Gallium" 69.723 0 0 []
+         , Element 32 "Ge" "Germanium" 72.61 0 0 []
+         , Element 33 "As" "Arsenic" 74.92159 0 0 []
+         , Element 34 "Se" "Selenium" 78.96 0 0 []
+         , Element 35 "Br" "Bromine" 79.904 0 0 []
+         , Element 36 "Kr" "Krypton" 83.8 0 0 []
+         , Element 37 "Rb" "Rubidium" 85.4678 0 0 []
+         , Element 38 "Sr" "Strontium" 87.62 0 0 []
+         , Element 39 "Y" "Yttrium" 88.90585 0 0 []
+         , Element 40 "Zr" "Zirconium" 91.224 0 0 []
+         , Element 41 "Nb" "Niobium" 92.90638 0 0 []
+         , Element 42 "Mo" "Molybdenum" 95.94 0 0 []
+         , Element 43 "Tc" "Technetium" 98.9063 0 0 []
+         , Element 44 "Ru" "Ruthenium" 101.07 0 0 []
+         , Element 45 "Rh" "Rhodium" 102.9055 0 0 []
+         , Element 46 "Pd" "Palladium" 106.42 0 0 []
+         , Element 47 "Ag" "Silver" 107.8682 0 0 []
+         , Element 48 "Cd" "Cadmium" 112.411 0 0 []
+         , Element 49 "In" "Indium" 114.82 0 0 []
+         , Element 50 "Sn" "Tin" 118.71 0 0 []
+         , Element 51 "Sb" "Antimony" 121.75 0 0 []
+         , Element 52 "Te" "Tellurium" 127.6 0 0 []
+         , Element 53 "I" "Iodine" 126.90447 0 0 []
+         , Element 54 "Xe" "Xenon" 131.29 0 0 []
+         , Element 55 "Cs" "Caesium" 132.90543 0 0 []
+         , Element 56 "Ba" "Barium" 137.327 0 0 []
+         , Element 57 "La" "Lanthanum" 138.9055 0 0 []
+         , Element 58 "Ce" "Cerium" 140.115 0 0 []
+         , Element 59 "Pr" "Praseodymium" 140.90765 0 0 []
+         , Element 60 "Nd" "Neodymium" 144.24 0 0 []
+         , Element 61 "Pm" "Promethium" 146.9151 0 0 []
+         , Element 62 "Sm" "Samarium" 150.36 0 0 []
+         , Element 63 "Eu" "Europium" 151.965 0 0 []
+         , Element 64 "Gd" "Gadolinium" 157.25 0 0 []
+         , Element 65 "Tb" "Terbium" 158.92534 0 0 []
+         , Element 66 "Dy" "Dysprosium" 162.5 0 0 []
+         , Element 67 "Ho" "Holmium" 164.93032 0 0 []
+         , Element 68 "Er" "Erbium" 167.26 0 0 []
+         , Element 69 "Tm" "Thulium" 168.93421 0 0 []
+         , Element 70 "Yb" "Ytterbium" 173.04 0 0 []
+         , Element 71 "Lu" "Lutetium" 174.967 0 0 []
+         , Element 72 "Hf" "Hafnium" 178.49 0 0 []
+         , Element 73 "Ta" "Tantalum" 180.9479 0 0 []
+         , Element 74 "W" "Tungsten" 183.85 0 0 []
+         , Element 75 "Re" "Rhenium" 186.207 0 0 []
+         , Element 76 "Os" "Osmium" 190.2 0 0 []
+         , Element 77 "Ir" "Iridium" 192.22 0 0 []
+         , Element 78 "Pt" "Platinum" 195.08 0 0 []
+         , Element 79 "Au" "Gold" 196.96654  0 0 []
+         , Element 80 "Hg" "Mercury" 200.59  0 0 []
+         , Element 81 "Tl" "Thallium" 204.3833 0 0 []
+         , Element 82 "Pb" "Lead" 207.2 0 0 []
+         , Element 83 "Bi" "Bismuth" 208.98037 0 0 []
+         , Element 84 "Po" "Polonium" 208.9824 0 0 []
+         , Element 85 "At" "Astatine" 209.9871 0 0 []
+         , Element 86 "Rn" "Radon" 222.0176 0 0 []
+         , Element 87 "Fr" "Francium" 223.0197 0 0 []
+         , Element 88 "Ra" "Radium" 226.0254 0 0 []
+         , Element 89 "Ac" "Actinium" 227.0278 0 0 []
+         , Element 90 "Th" "Thorium" 232.0381 0 0 []
+         , Element 91 "Pa" "Protactinium" 231.0359 0 0 []
+         , Element 92 "U" "Uranium" 238.0289 0 0 []
+         , Element 93 "Np" "Neptunium" 237.0482 0 0 []
+         , Element 94 "Pu" "Plutonium" 244.0642 0 0 []
+         , Element 95 "Am" "Americium" 243.0614 0 0 []
+         , Element 96 "Cm" "Curium" 247.0703 0 0 []
+         , Element 97 "Bk" "Berkelium" 247.0703 0 0 []
+         , Element 98 "Cf" "Californium" 251.0796 0 0 []
+         , Element 99 "Es" "Einsteinium" 252.0829 0 0 []
+         , Element 100 "Fm" "Fermium" 257.0951 0 0 []
+         , Element 101 "Md" "Mendelevium" 258.0986 0 0 []
+         , Element 102 "No" "Nobelium" 259.1009 0 0 []
+         , Element 103 "Lr" "Lawrencium" 260.1053 0 0 []
+         , Element 104 "Rf" "Rutherfordium" 261.1087 0 0 []
+         , Element 105 "Db" "Dubnium" 262.1138 0 0 []
+         , Element 106 "Sg" "Seaborgium" 263.1182 0 0 []
+         , Element 107 "Bh" "Bohrium" 262.1229 0 0 []
+         , Element 108 "Hs" "Hassium" 265 0 0 []
+         , Element 109 "Mt" "Meitnerium" 266 0 0 []
+         , Element 110 "Ds" "Darmstadtium" 269 0 0 []
+         , Element 111 "Rg" "Roentgenium" 272 0 0 []
+         , Element 112 "Cn" "Copernicium" 285 0 0 []
+         , Element 113 "Uut" "Ununtrium" 284 0 0 []
+         , Element 114 "Uug" "Ununquadium" 289 0 0 []
+         , Element 115 "Uup" "Ununpentium" 288 0 0 []
+         , Element 116 "Uuh" "Ununhexium" 293 0 0 []
+         , Element 117 "Uus" "Ununseptium" 294  0 0 []
+         , Element 118 "Uuo" "Ununoctium" 294 0 0 []]
            
 
 -- | Find element by its atomic number
@@ -160,10 +165,11 @@ elementBySymbol ns = f ns ptable
 -- > let e = element 8
 -- > shellElectrons e == [2, 6] 
 shellElectrons :: Element -> [Int]
+shellElectrons (Element _ _ _ _ _ _ (x:xs)) = x:xs
 shellElectrons e = filter (> 0) $ f (fillShells (atomicNumber e)) 
         where f :: [(Int, Int, Int)] -> [Int]
               f ss = [sum (g n ss) | n <- [1..m]]
-                where m = (length ss + 1) `div` 2
+                where m = length ss
                       g l = map (\(a,_,c) -> if a == l then c else 0)
                       
 -- | Number of valance electrons
--- test-src/Chemistry/ElementSpec.hs
+++ test-src/Chemistry/ElementSpec.hs
@@ -8,22 +8,30 @@ spec :: Spec
 spec = do
 
   describe "Element by atomic number" $ 
-    it "Element by 12" $ do
+    it "Element by 12" $ 
         atomicNumber (element 12) `shouldBe` 12
 
   describe "Element by name" $ 
-    it "Oxygen atomic number is 8" $ do
+    it "Oxygen atomic number is 8" $ 
         atomicNumber (elementBySymbol "O") `shouldBe` 8
 
   describe "Count shell electrons" $ 
-    it "Radium has 7 shells" $ do
+    it "Radium has 7 shells" $ 
         shellElectrons (elementBySymbol "Ra") `shouldBe` [2, 8, 18, 32, 18, 8, 2]
 
+  describe "Count shell electrons" $ 
+    it "Calcium has " $ 
+        shellElectrons (element 19) `shouldBe` [2, 8, 8, 1]
+
+  describe "Count shell electrons in metal" $ 
+    it "Chromium has " $ 
+        shellElectrons (element 24) `shouldBe` [2, 8, 13, 1]
+
   describe "Count valance electrons" $ 
-    it "Caesium has 1 valance electron" $ do
+    it "Caesium has 1 valance electron" $ 
         valanceElectrons (elementBySymbol "Cs") `shouldBe` 1
 
   describe "Count covalent bounds" $ 
-    it "Caesium has 1 bound" $ do
+    it "Caesium has 1 bound" $ 
         covalentBounds (elementBySymbol "Cs") `shouldBe` 1
         

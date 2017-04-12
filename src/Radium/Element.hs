39b66f73cb0d98e18f5048bcba750fe9c9c05980
Artem Kondyukov <artemkondyukov@gmail.com>
2017-04-12 14:13:00 +0300

    Ground/excited states of electron layers are implemented. Linter and stylish-haskell suggestions applied.

--- src/Radium/Element.hs
+++ src/Radium/Element.hs
@@ -12,48 +12,58 @@ This module contains Periodic Table with information about all known elements.
 
 module Radium.Element ( Element
                       , atomicNumber
-                      , symbol
                       , atomWeight
-                      , electroNegativity
-                      , ionizationEnergy
+                      , covalentBounds
                       , electronConfig
+                      , electroNegativity
                       , element
                       , elementBySymbol
+                      , ionizationEnergy
+                      , possibleElectronConfigs
+                      , possibleValences
+                      , symbol
                       , valanceElectrons
-                      , covalentBounds ) where
-                         
-import qualified Data.Map as Map                       
+                      ) where
+
+import           Control.Arrow ((&&&))
+import           Control.Lens  (Field1 (..), Field2 (..), Field3 (..), (^.))
+import           Data.Foldable (toList)
+import           Data.List     (findIndex, findIndices, unfoldr)
+import qualified Data.Map      as Map
+import           Data.Maybe    (listToMaybe)
+import           Data.Sequence (fromList, update)
+import           Debug.Trace   (trace)
 
 
-data Element = Element { atomicNumber :: Int
-                       , symbol :: String
-                       , _name :: String
-                       , atomWeight :: Double 
+data Element = Element { atomicNumber      :: Int
+                       , symbol            :: String
+                       , _name             :: String
+                       , atomWeight        :: Double
                        , electroNegativity :: Double
-                       , ionizationEnergy :: Double
-                       } 
+                       , ionizationEnergy  :: Double
+                       }
              | Unknown deriving (Eq, Show)
-            
+
 -- Periodic Table
 ptable :: [Element]
-ptable = [ Element 1 "H" "Hydrogen"         1.008       2.2     1.312  
-         , Element 2 "He" "Helium"          4.002602    0       2372.3 
-         , Element 3 "Li" "Lithium"         6.941       0.98    520.2  
-         , Element 4 "Be" "Beryllium"       9.012182    1.57    899.5  
-         , Element 5 "B" "Boron"            10.811      2.04    800.6  
-         , Element 6 "C" "Carbon"           12.011      2.55    1086.5 
-         , Element 7 "N" "Nitrogen"         14.007      3.04    1402.3 
-         , Element 8 "O" "Oxygen"           15.999      3.44    1313.9 
-         , Element 9 "F" "Fluorine"         18.9984032  3.98    1681   
-         , Element 10 "Ne" "Neon"           20.1797     0       2080.7 
-         , Element 11 "Na" "Sodium"         22.989768   0.93    495.8  
-         , Element 12 "Mg" "Magnesium"      24.305      1.31    737.7  
-         , Element 13 "Al" "Alluminium"     26.981      1.61    577.5  
-         , Element 14 "Si" "Silicon"        28.085      1.9     786.5  
-         , Element 15 "P" "Phosphous"       30.973      2.19    1011.8 
-         , Element 16 "S" "Sulphur"         32.066      2.58    999.6  
-         , Element 17 "Cl" "Chlorine"       35.4527     3.16    1251.2 
-         , Element 18 "Ar" "Argon"          39.948      0       1520.6 
+ptable = [ Element 1 "H" "Hydrogen"         1.008       2.2     1.312
+         , Element 2 "He" "Helium"          4.002602    0       2372.3
+         , Element 3 "Li" "Lithium"         6.941       0.98    520.2
+         , Element 4 "Be" "Beryllium"       9.012182    1.57    899.5
+         , Element 5 "B" "Boron"            10.811      2.04    800.6
+         , Element 6 "C" "Carbon"           12.011      2.55    1086.5
+         , Element 7 "N" "Nitrogen"         14.007      3.04    1402.3
+         , Element 8 "O" "Oxygen"           15.999      3.44    1313.9
+         , Element 9 "F" "Fluorine"         18.9984032  3.98    1681
+         , Element 10 "Ne" "Neon"           20.1797     0       2080.7
+         , Element 11 "Na" "Sodium"         22.989768   0.93    495.8
+         , Element 12 "Mg" "Magnesium"      24.305      1.31    737.7
+         , Element 13 "Al" "Alluminium"     26.981      1.61    577.5
+         , Element 14 "Si" "Silicon"        28.085      1.9     786.5
+         , Element 15 "P" "Phosphous"       30.973      2.19    1011.8
+         , Element 16 "S" "Sulphur"         32.066      2.58    999.6
+         , Element 17 "Cl" "Chlorine"       35.4527     3.16    1251.2
+         , Element 18 "Ar" "Argon"          39.948      0       1520.6
          , Element 19 "K" "Potassium"       39.0983     0.82    418.8
          , Element 20 "Ca" "Calcium"        40.078      1       589.8
          , Element 21 "Sc" "Scandium"       44.95591    1.36    633.1
@@ -98,7 +108,7 @@ ptable = [ Element 1 "H" "Hydrogen"         1.008       2.2     1.312
          , Element 60 "Nd" "Neodymium"      144.24      1.14    533.1
          , Element 61 "Pm" "Promethium"     146.9151    0       540
          , Element 62 "Sm" "Samarium"       150.36      1.17    544.5
-         , Element 63 "Eu" "Europium"       151.965     0       547.1       
+         , Element 63 "Eu" "Europium"       151.965     0       547.1
          , Element 64 "Gd" "Gadolinium"     157.25      1.2     593.4
          , Element 65 "Tb" "Terbium"        158.92534   0       565.8
          , Element 66 "Dy" "Dysprosium"     162.5       1.22    573
@@ -130,7 +140,7 @@ ptable = [ Element 1 "H" "Hydrogen"         1.008       2.2     1.312
          , Element 92 "U" "Uranium"         238.0289    1.38    597.6
          , Element 93 "Np" "Neptunium"      237.0482    1.36    604.5
          , Element 94 "Pu" "Plutonium"      244.0642    1.28    584.7
-         , Element 95 "Am" "Americium"      243.0614    1.3     578 
+         , Element 95 "Am" "Americium"      243.0614    1.3     578
          , Element 96 "Cm" "Curium"         247.0703    1.3     581
          , Element 97 "Bk" "Berkelium"      247.0703    1.3     601
          , Element 98 "Cf" "Californium"    251.0796    1.3     608
@@ -155,11 +165,11 @@ ptable = [ Element 1 "H" "Hydrogen"         1.008       2.2     1.312
          , Element 117 "Ts" "Tennessine"    294         0       0
          , Element 118 "Og" "Oganesson"     294         0       0
          ]
-           
+
 
 -- | Find element by its atomic number
 --
--- > atomicNumber (element 8) == 8 
+-- > atomicNumber (element 8) == 8
 element :: Int -> Element
 element n = f n ptable
     where f :: Int -> [Element] -> Element
@@ -169,24 +179,24 @@ element n = f n ptable
 
 -- | Find element by its symbol
 --
--- > atomicNumber (elementBySymbol "O") == 8 
+-- > atomicNumber (elementBySymbol "O") == 8
 elementBySymbol :: String -> Element
 elementBySymbol ns = f ns ptable
     where f :: String -> [Element] -> Element
           f _ [] = Unknown
           f xs (e:es) | symbol e == xs = e
-                     | otherwise = f xs es
+                      | otherwise = f xs es
 
 -- Electron configuration exceptions to Aufbau principle
-configExceptions :: Map.Map Int [Int] 
+configExceptions :: Map.Map Int [Int]
 configExceptions = Map.fromList [ (24, [2, 8, 13, 1])
-                                , (29, [2, 8, 18, 1]) 
+                                , (29, [2, 8, 18, 1])
                                 , (41, [2, 8, 18, 12, 1])
                                 , (42, [2, 8, 18, 13, 1])
                                 , (44, [2, 8, 18, 15, 1])
                                 , (45, [2, 8, 18, 16, 1])
                                 , (46, [2, 8, 18, 18])
-                                , (47, [2, 8, 18, 18, 1]) 
+                                , (47, [2, 8, 18, 18, 1])
                                 , (57, [2, 8, 18, 18, 9, 2])
                                 , (58, [2, 8, 18, 19, 9, 2])
                                 , (64, [2, 8, 18, 25, 9, 2])
@@ -195,64 +205,125 @@ configExceptions = Map.fromList [ (24, [2, 8, 13, 1])
                                 , (90, [2, 8, 18, 32, 18, 10, 2])
                                 , (91, [2, 8, 18, 32, 20, 9, 2])
                                 , (92, [2, 8, 18, 32, 21, 9, 2])
-                                , (93, [2, 8, 18, 32, 22, 9, 2]) 
+                                , (93, [2, 8, 18, 32, 22, 9, 2])
                                 , (96, [2, 8, 18, 32, 25, 9, 2]) ]
-                                        
+
 -- | Show number of electrons in each shell
 --   For elements which are exception to Aufbau principle configuration is given manually
---   Is it possible to calculate it for all elements based only on atom properties?             
+--   Is it possible to calculate it for all elements based only on atom properties?
 --
 -- > let e = element 8
--- > shellElectrons e == [2, 6] 
+-- > shellElectrons e == [2, 6]
 electronConfig :: Element -> [Int]
 electronConfig e = case Map.lookup (atomicNumber e) configExceptions of
                     Just val -> val
-                    _ -> filter (> 0) $ f (fillShells (atomicNumber e)) 
+                    _ -> filter (> 0) $ f (fillShells (atomicNumber e))
                         where f :: [(Int, Int, Int)] -> [Int]
                               f ss = [sum (g n ss) | n <- [1..m]]
                                 where m = length ss
                                       g l = map (\(a,_,c) -> if a == l then c else 0)
-                      
+
 -- | Number of valance electrons
 --
 -- > let e = element 8
--- > valanceElectrons e == 6 
+-- > valanceElectrons e == 6
 valanceElectrons :: Element -> Int
 valanceElectrons e = last (electronConfig e)
 
 -- | Number of covalent bounds in element
 --
 -- > let e = element 8
--- > covalentBounds e == 2 
+-- > covalentBounds e == 2
 covalentBounds :: Element -> Int
 covalentBounds e = min n (8-n)
-    where n = valanceElectrons e
+  where n = valanceElectrons e
+
+
+-- Get list of all possible coordination numbers (valences)
+possibleValences :: Element -> [Int]
+possibleValences el = fmap helper . possibleElectronConfigs $ atomicNumber el
+  where
+    helper = foldr val 0
+    valSub s e = if e < size then e else size * 2 - e
+      where size = subshellOrbitals !! (s - 1)
+    val (_, s, e) b = b + valSub s e
 
 
 -- How many electrons are in each subshell
 subshellMaxElectrons :: [Int]
-subshellMaxElectrons = [2, 6, 10, 14, 18]
+subshellMaxElectrons = fmap (*2) subshellOrbitals
+
+subshellOrbitals :: [Int]
+subshellOrbitals = [1, 3, 5, 7, 9]
+
+
+-- Turn atom into the next excited state
+excite :: [(Int, Int, Int)] -> Maybe [(Int, Int, Int)]
+excite conf = fmap removeEmptyShells excConf
+  where
+    highestPaired :: [(Int, Int, Int)] -> Maybe Int
+    highestPaired = listToMaybe . reverse . findIndices (\(_, s, e) -> (subshellOrbitals !! (s - 1)) < e)
+
+    lowestFree :: [(Int, Int, Int)] -> Maybe Int
+    lowestFree = findIndex (\(_, s, e) -> (subshellOrbitals !! (s - 1)) > e)
+
+    increase (l, s, e) = (l, s, e + 1)
+    decrease (l, s, e) = (l, s, e - 1)
+
+    expConf = appendEmptyShells conf
+
+    excConf = case (highestPaired &&& lowestFree) expConf of
+      (Nothing, _)     -> Nothing
+      (_, Nothing)     -> Nothing
+      (Just h, Just l) -> if (conf !! h) ^._1 == (expConf !! l) ^._1
+                          then
+                            Just
+                              . toList
+                              . update l (increase $ expConf !! l)
+                              . update h (decrease $ expConf !! h)
+                              $ fromList expConf
+                          else
+                            Nothing
+
+-- Generate all possible electron configs (unexcited and excited) with given number of electrons
+possibleElectronConfigs :: Int -> [[(Int, Int, Int)]]
+possibleElectronConfigs e = fillShells e : unfoldr helper (fillShells e)
+  where
+    helper x = do
+      res <- excite x
+      return (res, res)
 
 -- Generate possible config for given maximum number of shells.
--- The order is based on Aufbau principle. 
+-- The order is based on Aufbau principle.
 -- http://en.wikipedia.org/wiki/Aufbau_principle
 shellConfigGen :: Int -> [(Int, Int)]
-shellConfigGen n = [(i,m-i) | m <- [2..n+n], i <- [((m+1) `div` 2)..m-1] ] 
+shellConfigGen n = [(i,m-i) | m <- [2..n+n], i <- [((m+1) `div` 2)..m-1] ]
+
+
+-- Appends sublayers which don't carry any electron in the ground state
+-- but could carry in excited state
+appendEmptyShells :: [(Int, Int, Int)] -> [(Int, Int, Int)]
+appendEmptyShells conf = conf ++ addSublayers
+  where
+    maxLayer = maximum (fmap (^._1) conf)
+    maxSublayer = maximum ((^._2) <$> filter ((==maxLayer) . (^._1)) conf)
+    addSublayers = fmap (\x -> (maxLayer, x, 0)) [(maxSublayer + 1) .. maxLayer]
+
+
+-- Do the oppsoite of the function above
+removeEmptyShells :: [(Int, Int, Int)] -> [(Int, Int, Int)]
+removeEmptyShells = filter ((/= 0) . (^._3))
+
 
 -- Fill shells with given number of electrons
 fillShells :: Int -> [( Int  -- Shell number
-                    , Int  -- Shell type
-                    , Int  -- #Electrons
-                      )]
+                      , Int  -- Shell type
+                      , Int  -- #Electrons
+                        )]
 fillShells = f (shellConfigGen 5)
         where f :: [(Int, Int)] -> Int -> [(Int, Int, Int)]
               f [] _ = []
               f ((i,j):xs) m | m == 0 = []
                              | m < l = [(i, j, m)]
                              | otherwise = (i, j, l) : f xs (m - l)
-                             where l = subshellMaxElectrons !! (j-1)  
-                                   
-                 
-
-              
-              
+                             where l = subshellMaxElectrons !! (j-1)
